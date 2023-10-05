8a15993b391edea6c35fed17da0a1237c68bc66a
JCMyth <s11556990@students.duvalschools.org>
2023-10-05 13:44:20 -0400

    v0.1b --
    Print elements from array on a single line.

--- multiUserGameProgramming/computer_science_exercises/02_collections/Program.cs
+++ multiUserGameProgramming/computer_science_exercises/02_collections/Program.cs
@@ -1,4 +1,8 @@
-﻿// 02_Collections, Julian Cunningham, 10/3/23 v0.1a
+﻿using System.Reflection.Emit;
+using System.Data;
+using System.Text;
+using Internal;
+// 02_Collections, Julian Cunningham, 10/3/23 v0.1a
 
 using System;
 
@@ -21,9 +25,17 @@ namespace _02_collections
 
             // Declaring and Defining an Array
             string[] breakfastFoods = {"Bacon", "Waffles", "Pancakes", "cereal", "Parfait"};
-            int[] = testScores = {95, 100, 25, 15, 27, 35};
-            float[] = GPA = {3.14f, 2.25f, 1.74f, 1.99f, 099f, 4.25f};
+            int[] testScores = {95, 100, 25, 15, 27, 35};
+            float[] GPA = {3.14f, 2.25f, 1.74f, 1.99f, 099f, 4.25f};
 
+            // Print Array Contents -- All Elements on a Single Line
+            Console.WriteLine("The elements for each array are:\n");
+            Console.WriteLine("breakfastFoods: \n" + string.Join(", ", breakfastFoods));
+            Console.WriteLine();
+            Console.WriteLine("testScores: \n" + string.Join(", ", testScores));
+            Console.WriteLine();
+            Console.WriteLine("GPA: \n" + string.Join(", ", GPA));
+            Console.WriteLine();
 
         }
     }
