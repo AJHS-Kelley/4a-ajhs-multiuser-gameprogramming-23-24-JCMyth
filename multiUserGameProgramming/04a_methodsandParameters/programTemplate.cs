2f0a4a71bca137e0477729f21418a5db6896cfd4
JCMyth <s11556990@students.duvalschools.org>
2023-10-24 14:10:00 -0400

    v0.01
    added signature

---
+++ multiUserGameProgramming/04a_methodsandParameters/programTemplate.cs
@@ -0,0 +1,23 @@
+// Julian Cunningham , Program Template, v0.01
+using System;
+
+namespace MethodsParameters
+{
+    class MethodsParameters
+    {
+        // METHOD -- named block of code, can be used over and over.
+        // All methods have a SIGNATURE that defines their name, parameters, and output.
+        // Example Signature
+        static void MyMethod()
+        {
+            Console.WriteLine("The game costed about $80 with add-ons.");
+        }
+        static void Main(string[] args)
+        {
+           MyMethod();
+        }
+    }
+
+
+
+}
