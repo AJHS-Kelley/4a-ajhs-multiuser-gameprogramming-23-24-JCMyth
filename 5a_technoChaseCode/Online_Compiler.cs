2f328becfbff5ab3b99152505752a4d0484ff990
JCMyth <s11556990@students.duvalschools.org>
2023-11-15 11:41:04 -0500

    update
    tried adding an Attack variable to change the outcome

--- 5a_technoChaseCode/Online_Compiler.cs
+++ 5a_technoChaseCode/Online_Compiler.cs
@@ -1,4 +1,5 @@
-/* The purpose of this code is to test segments of my original code without risking damage to my actual code. as well as function as a note pad
+/*
+The purpose of this code is to test segments of my original code without risking damage to my actual code. as well as function as a note pad
 Ideas for segments
     Add a segment that focuses on the damage of the critical attack
     Critical Attack has a chance to activate "ZONE" (True or false)
@@ -19,14 +20,14 @@ namespace ExampleTests
     {
         
         // Code to initialize whether a Critical attack is landed or not.
-        static int CritAttack(boost)
+        static int CritAttack(Attack)
         {
             //Set up Critical attack chance
             int CritAttack; Random random = new Random();
             for (int i = 0; i < 2; i++)
             {
                 Console.WriteLine("Press any key to attack./n");
-                 
+                string Attack;
                 Console.ReadKey();
                 CritAttack = random.Next(1, 6);
                 // IF statement determines if the attack if critical, and assigns it a string name that can be used later.
