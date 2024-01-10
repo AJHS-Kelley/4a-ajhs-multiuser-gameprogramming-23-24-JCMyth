191142f6677663bc93e65985f4a0d87f3d85616b
JCMyth <s11556990@students.duvalschools.org>
2024-01-10 14:05:05 -0500

    review
    reviewed basic C#

--- 06_review/programTemplate.cs
+++ 06_review/programTemplate.cs
@@ -1,12 +1,41 @@
 // Julian Cunningham , Program Templae, v0.01
 using System;
 
-namespace UPDATEFOREACHPROGRAM
+namespace ReviewProject
 {
-    class UPDATEFOREACHPROGRAM
+    class ReviewProject
     {
         static void Main(string[] args)
         {
+            //cDeclaring and Assigning Variables
+            // Integer = 4 Bytes
+           int hits = 1;
+
+           // Long -- -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+           // Long -- 8 Bytes
+           long myLong = 1928742747242894;
+
+           //Float -- Stores up to 6-7 decimal digits
+           // Float = 4 Bytes 
+           float height = 2.4f;
+
+           // Double -- Stores up to 155 decimal digits
+           // Double = 8 Bytes
+           double myDouble = 9.123456789102345D;
+
+           // Boolean -- true / false
+           // Boolean = 1 bit
+           bool landed = true;
+
+           // String
+           // 2-Bytes per character
+           // Must be in DOUBLE QUOTES " "
+           string noString = "Boo!";
+
+           // Character (char)
+           // Signle character, in SINGLE QUOTES ' ' 
+           // 2-Bytes
+           char myChar = 'C';
            
         }
     }
