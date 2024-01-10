d8d7c19db014bb18c7cbf2931cdde5e63da9f304
JCMyth <s11556990@students.duvalschools.org>
2024-01-10 14:47:45 -0500

    more updates

--- 06_review/programTemplate.cs
+++ 06_review/programTemplate.cs
@@ -21,7 +21,7 @@ namespace ReviewProject
 
            // Double -- Stores up to 155 decimal digits
            // Double = 8 Bytes
-           double myDouble = 9.123456789102345D;
+           double ourDouble = 9.123456789102345D;
 
            // Boolean -- true / false
            // Boolean = 1 bit
@@ -36,6 +36,57 @@ namespace ReviewProject
            // Signle character, in SINGLE QUOTES ' ' 
            // 2-Bytes
            char myChar = 'C';
+
+           // Type Casting -- changing one data type to another
+           // IMPLICIT -- happens automatically when going smaller -> larger
+           // char -> int -> long -> float -> double
+           int myint = 9;
+           double myDouble = myint;
+           Console.WriteLine(myDouble);
+
+
+           // EXPLICIT -- Must be done manually when going larger -> smaller
+           // double -> float -> long -> int -> char
+           double exampleDouble = 9.756212345D;
+           int myInt = (int) myDouble;
+
+           // Conversion Methods
+           string myString = Convert.ToString(myint); // String
+           float newHeight = Convert.ToFloat(height); // Float
+           Convert.ToInt32(); // Integer
+           Convert.ToInt64(); // Long
+
+           // Switch Statement
+           int dayOfWeek = 5;
+           switch (dayOfWeek)
+           {
+                case 1:
+                    Console.WriteLine("The day is Sunday.\n");
+                    break;
+                case 2:
+                    Console.WriteLine("The day is Monday.\n");
+                    break;
+                case 3:
+                    Console.WriteLine("The day is Tuesday.\n");
+                    break;
+                case 4:
+                    Console.WriteLine("The day is Wednesday.\n");
+                    break;
+                case 5:
+                    Console.WriteLine("The day is Thursday.\n");
+                    break;
+                case 5:
+                    Console.WriteLine("The day is Friday.\n");
+                    break;
+                case 7:
+                    Console.WriteLine("The day is Saturday.\n");
+                    break;
+                default:
+                    Console.WriteLine("No match found.\n")
+                    break;
+
+           }
+
            
         }
     }
@@ -43,3 +94,5 @@ namespace ReviewProject
 
 
 }
+
+
